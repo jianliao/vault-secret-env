72c44c1bed7cd5c156bd6ecb04651b9d66ce6b75
jianliao <jian.liao@gmail.com>
2022-08-13 13:33:28 -0700

    Fixed core func

--- src/core.js
+++ src/core.js
@@ -2,7 +2,7 @@ import { execFileSync } from 'child_process';
 import { join } from 'path';
 import * as url from 'url';
 
-const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url)); // To mimic the __dirname global variable in commonjs
 
 async function getVaultSecrets(vaultAddr, rootToken, secretsByPath, tokenRole = null, ttl = 60) {
   let token = rootToken;
@@ -25,29 +25,25 @@ async function getVaultSecrets(vaultAddr, rootToken, secretsByPath, tokenRole = 
   const secretsKV = {};
 
   for (let secretPath in secretsByPath) {
-    secretPath = secretPath.endsWith('/') ? secretPath.slice(0, -1) : secretPath; // remove last slash
-
-    let response = JSON.parse(sendMessage({
-      url: `${vaultAddr}/v1/${secretPath}`,
-      headers: { 'X-Vault-Token': token },
-      method: 'GET'
-    }));
-
-    if (response.error) {
-      console.error(response.error);
-      continue;
-    }
-
-    let data = response.data;
-    if (data.data) {
-      data = data.data;
-    }
-
-    for (const secretKey in secretsByPath[secretPath]) {
-      const secretValue = data[secretKey];
-      if (secretValue) {
-        secretsKV[secretKey] = secretValue;
+    secretPath = secretPath.endsWith('/') ? secretPath.slice(0, -1) : secretPath; // remove the last slash
+    try {
+      const res = await fetch(`${vaultAddr}/v1/${secretPath}`, { headers: { 'X-Vault-Token': token }, method: 'GET' });
+      if (res.ok) {
+        let { data } = await res.json();
+        if (data.data) {
+          data = data.data;
+        }
+        for (const secretKey in secretsByPath[secretPath]) {
+          const secretValue = data[secretKey];
+          if (secretValue) {
+            secretsKV[secretKey] = secretValue;
+          }
+        }
+      } else {
+        console.error(res.statusText);
       }
+    } catch (e) {
+      console.error(e.message);
     }
   }
 
@@ -70,14 +66,20 @@ function getVaultSecretsSync(vaultAddr, rootToken, secretsByPath, tokenRole = nu
   const secretsKV = {};
 
   for (let secretPath in secretsByPath) {
-    secretPath = secretPath.endsWith('/') ? secretPath.slice(0, -1) : secretPath; // remove last slash
+    secretPath = secretPath.endsWith('/') ? secretPath.slice(0, -1) : secretPath; // remove the last slash
 
-    let { data } = JSON.parse(sendMessage({
+    let response = JSON.parse(sendMessage({
       url: `${vaultAddr}/v1/${secretPath}`,
       headers: { 'X-Vault-Token': token },
       method: 'GET'
     }));
 
+    if (response.error) {
+      console.error(response.error);
+      continue;
+    }
+
+    let data = response.data;
     if (data.data) {
       data = data.data;
     }
